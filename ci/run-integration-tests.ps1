a23a53fd4effb345a0f7224fccd2f37d2a2e1d11
Alexander Popel <alexander.popel@postindustria.com>
2023-07-17 12:00:04 +0300

    Support CI build without a resource key

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -5,6 +5,11 @@ param (
     [hashtable]$Keys
 )
 
+if (!$Keys.TestResourceKey) {
+    Write-Output "::warning file=$($MyInvocation.ScriptName),line=$($MyInvocation.ScriptLineNumber),title=No Resource Key::No resource key was provided, so integration tests will not run."
+    return
+}
+
 $env:RESOURCEKEY = $Keys.TestResourceKey
 
 ./php/run-integration-tests.ps1 -RepoName $RepoName
